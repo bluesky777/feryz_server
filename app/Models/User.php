db0d1b8d7b2b89ce83a27c3ee6f0f9b74650e10a
blesky777 <davidguerrero777@gmail.com>
2021-09-13 12:54:11 -0500

    Actulizo user cn jwt

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,13 +5,15 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 use DB;
 
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use SoftDeletes;
+    use Notifiable;
 
     protected $softDelete = true;
     
@@ -102,5 +104,29 @@ class User extends Authenticatable
 
         return $user;
     }
+
+    
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+
+
 
 }
